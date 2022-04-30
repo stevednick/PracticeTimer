5fa8a66186571d9d09396fa62ca55ca7c26c365a
stevednick <stephendnicholls@yahoo.co.uk>
2022-04-30 19:18:55 +0100

    Initial Commit

---
+++ PracticeTimer/PracticeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTimerApp.swift
+//  PracticeTimer
+//
+//  Created by Stephen Nicholls on 30/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct PracticeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
